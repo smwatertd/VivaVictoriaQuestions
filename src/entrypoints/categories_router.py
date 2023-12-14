eacb5885838453be2bc1983b8a0e217ab40109e2
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-14 22:50:55 +0500

    get category by id endpoint

--- src/entrypoints/categories_router.py
+++ src/entrypoints/categories_router.py
@@ -14,3 +14,9 @@ router = APIRouter(
 async def get_categories() -> list[CategorySchema]:
     categories = await services.get_all_categories()
     return [CategorySchema(id=category.id, name=category.name) for category in categories]
+
+
+@router.get('/categories/{category_id}', status_code=200, response_model=CategorySchema)
+async def get_category(category_id: int) -> CategorySchema:
+    category = await services.get_category(category_id)
+    return CategorySchema(id=category.id, name=category.name)
--- src/services.py
+++ src/services.py
@@ -6,3 +6,7 @@ class Category:
 
 async def get_all_categories() -> list[Category]:
     return [Category(id=i, name=f'Category {i}') for i in range(10)]
+
+
+async def get_category(category_id: int) -> Category:
+    return Category(id=category_id, name=f'Category {category_id}')
