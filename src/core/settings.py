5f496f139fd6aba70ae46cbad3bbd04a72e3131b
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-15 18:14:56 +0500

    add DatabaseSettings

--- src/core/settings.py
+++ src/core/settings.py
@@ -12,4 +12,24 @@ class AppSettings(BaseSettings):
     )
 
 
+class DatabaseSettings(BaseSettings):
+    driver: str = 'postgresql+asyncpg'
+    options: str = '?async_fallback=True'
+    host: str = 'localhost'
+    port: int = 5432
+    user: str = 'postgres'
+    password: str = 'postgres'
+    database: str = 'postgres'
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_prefix='DB_',
+    )
+
+    @property
+    def url(self) -> str:
+        return f'{self.driver}://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}{self.options}'
+
+
 app_settings = AppSettings()
+db_settings = DatabaseSettings()
