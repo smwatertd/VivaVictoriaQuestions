69baacd1cb44a9e678dd713d556978e1ea202602
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-18 18:45:40 +0500

    add categories model

--- src/models.py
+++ src/models.py
@@ -1,5 +1,12 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
