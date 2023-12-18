0924b7d404eb23ac4da91c2d0d3fe802bfa6a982
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-18 23:39:40 +0500

    add get_random_question handler

--- src/entrypoints/questions_router.py
+++ src/entrypoints/questions_router.py
@@ -16,6 +16,15 @@ router = APIRouter(
 )
 
 
+@router.get('/questions/random', status_code=200)
+async def get_random_question(
+    category_id: Annotated[int, Query(description='Category ID')],
+    questions_service: Annotated[services.QuestionsService, Depends(dependencies.get_questions_service)],
+    uow: Annotated[UnitOfWork, Depends(dependencies.get_unit_of_work)],
+) -> QuestionSchema:
+    return await questions_service.get_random_question_by_category_id(category_id, uow)
+
+
 @router.get('/questions', status_code=200)
 async def get_questions_by_category(
     category_id: Annotated[int, Query(description='Category ID')],
