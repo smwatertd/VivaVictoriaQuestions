ad0e7acde07f4e0f3bc60df53f8bd91e7ac683b3
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-18 23:02:42 +0500

    add QuestionsService

--- src/services.py
+++ src/services.py
@@ -1,15 +1,27 @@
-from schemas import CategorySchema
+import schemas
 
 from unit_of_work import UnitOfWork
 
 
 class CategoriesService:
-    async def get_all_categories(self, uow: UnitOfWork) -> list[CategorySchema]:
+    async def get_all_categories(self, uow: UnitOfWork) -> list[schemas.CategorySchema]:
         async with uow:
             categories = await uow.categories.get_all()
             return [category.to_schema() for category in categories]
 
-    async def get_category(self, category_id: int, uow: UnitOfWork) -> CategorySchema:
+    async def get_category(self, category_id: int, uow: UnitOfWork) -> schemas.CategorySchema:
         async with uow:
             category = await uow.categories.get(category_id)
             return category.to_schema()
+
+
+class QuestionsService:
+    async def get_questions_by_category_id(
+        self,
+        category_id: int,
+        uow: UnitOfWork,
+    ) -> list[schemas.QuestionByCategorySchema]:
+        async with uow:
+            category = await uow.categories.get(category_id)
+            questions = await uow.questions.all_by_category_id(category.id)
+            return [schemas.QuestionByCategorySchema(id=question.id, body=question.body) for question in questions]
