636e1292051f6e588612eb0399a36ca2712823a3
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-18 20:40:31 +0500

    bugfix: add async with unit_of_work usage

--- src/services.py
+++ src/services.py
@@ -5,9 +5,11 @@ from unit_of_work import UnitOfWork
 
 class CategoriesService:
     async def get_all_categories(self, uow: UnitOfWork) -> list[CategorySchema]:
-        categories = await uow.categories.get_all()
-        return [category.to_schema() for category in categories]
+        async with uow:
+            categories = await uow.categories.get_all()
+            return [category.to_schema() for category in categories]
 
     async def get_category(self, category_id: int, uow: UnitOfWork) -> CategorySchema:
-        category = await uow.categories.get(category_id)
-        return category.to_schema()
+        async with uow:
+            category = await uow.categories.get(category_id)
+            return category.to_schema()
