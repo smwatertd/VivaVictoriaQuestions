b6cae11a99960dd0d1da66eb6bd863c0a300e0f2
smwatertd <korablev.danil.smtd@mail.ru>
2023-12-18 20:30:02 +0500

    add SQLAlchemyUnitOfWork

--- src/unit_of_work.py
+++ src/unit_of_work.py
@@ -1,7 +1,18 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+from core.settings import db_settings
+
 import repositories
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+
+DEFAULT_SESSION_MAKER = async_sessionmaker(
+    bind=create_async_engine(db_settings.url),
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 
 class UnitOfWork(ABC):
@@ -20,3 +31,19 @@ class UnitOfWork(ABC):
     @abstractmethod
     async def commit(self) -> None:
         pass
+
+
+class SQLAlchemyUnitOfWork(UnitOfWork):
+    def __init__(self, session_factory: async_sessionmaker[AsyncSession] = DEFAULT_SESSION_MAKER) -> None:
+        self._session_factory = session_factory
+
+    async def __aenter__(self) -> UnitOfWork:
+        self._session = self._session_factory()
+        self.categories = repositories.SQLAlchemyCategoriesRepository(self._session)
+        return await super().__aenter__()
+
+    async def rollback(self) -> None:
+        await self._session.rollback()
+
+    async def commit(self) -> None:
+        await self._session.commit()
